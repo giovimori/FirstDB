d4b7ec1d4197627fd99f2ec76f2034b9ee4d3048
Giovanni Morelli <116790906+giovimori@users.noreply.github.com>
2023-04-28 09:41:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SQLite;
+
+Console.WriteLine("Hello, DBWorld!");
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+var tblArtist = cn1.Query<Artist>( "select * from artists" );
+Console.WriteLine( $" In questa tabella ci sono {tblArtist.Count} record!");
+
+// ----------------------
+// fine del main inzio delle dichiarazioni 
+public class Artist
+{
+    public int ArtistId{get;set;}
+    public string Name{get;set;}
+}
